bbc3897969cf7d13fa26052783fa044ec08e9cc5
eruesu <56844000+eruesu@users.noreply.github.com>
2021-05-27 10:10:02 -0700

    preonic-eruesu: Adjust tapping term. Re-add Mod Tap for Escape

--- keyboards/preonic/keymaps/eruesu/config.h
+++ keyboards/preonic/keymaps/eruesu/config.h
@@ -1,5 +1,15 @@
 #pragma once
 
+// Number of registered combos
+#define COMBO_COUNT 1
+
+// Timeout for combos to be detected
+#define COMBO_TERM 125
+
+// Timeout for keypress to be considered a hold instead of tap
+#define TAPPING_TERM 150
+
+
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
@@ -12,17 +22,10 @@
 
 #define MUSIC_MASK (keycode != KC_NO)
 
-#define COMBO_COUNT 1
 
 /*
  * MIDI options
  */
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
-#define MIDI_BASIC
 
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
@@ -30,7 +33,7 @@
    - Virtual sustain, portamento, and modulation wheel
    - etc.
 */
-//#define MIDI_ADVANCED
+#define MIDI_ADVANCED
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
--- keyboards/preonic/keymaps/eruesu/readme.md
+++ keyboards/preonic/keymaps/eruesu/readme.md
@@ -1,13 +1,14 @@
 # Preonic Layout (eruesu)
 
 Default plank layout with modifications:
-- [X] Default left escape to control
+- [X] Default left escape to mod tap left control + escape
+- [X] Adjust combo and tapping term
 - [X] Remove DVORAK and COLEMAK layers
 - [X] Disable RGB lighting
 - [x] Add mod tap for right enter + right shift
 - [X] Default enter to right shift
 - [X] Default DEL to backslash
 - [X] Add JK combo for ESC
-- [X] Shift Default HJKL; keys up `_+{}-=[]`
+- [X] Shift Default `HJKL;` keys up `_+{}-=[]`
 - [X] Map HJKL to navigation keys
-- [ ] Another combination for ESC (Layer + GRV?)
+- [ ] Ignore JK combo if any modifiers are pressed
