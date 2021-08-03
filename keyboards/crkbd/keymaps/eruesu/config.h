93c01839bc76c2069e9a7336ae2a03b23341cf95
eruesu <56844000+eruesu@users.noreply.github.com>
2021-08-03 10:47:46 -0700

    crkbd-eruesu: Update to left-master

--- keyboards/crkbd/keymaps/eruesu/config.h
+++ keyboards/crkbd/keymaps/eruesu/config.h
@@ -24,9 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-// #define MASTER_LEFT
+#define MASTER_LEFT
 // #define MASTER_RIGHT
-#define EE_HANDS
+// #define EE_HANDS
 
 #define USE_SERIAL_PD2
 
@@ -38,7 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
     #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
