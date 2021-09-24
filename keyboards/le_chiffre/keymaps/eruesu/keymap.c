78d0105a1c2f661a6d5b68218314972bdaa20a5a
eruesu <56844000+eruesu@users.noreply.github.com>
2021-09-24 09:55:42 -0700

    le_chiffre: Space Layer keys

--- keyboards/le_chiffre/keymaps/eruesu/keymap.c
+++ keyboards/le_chiffre/keymaps/eruesu/keymap.c
@@ -17,8 +17,8 @@
 
 enum layers{
   _BASE,
-  _NUM_SYM,
-  _NAV
+  _LOWER,
+  _RAISE
 };
 
 enum combo_events {
@@ -29,7 +29,8 @@ enum combo_events {
   COMBO_DEL,
 };
 
-#define KC_NUM_SPC LT(_NUM_SYM, KC_SPC)
+#define KC_SPC_LWR LT(_LOWER, KC_SPC)
+#define KC_SPC_RSE LT(_RAISE, KC_SPC)
 #define KC_GA LGUI_T(KC_A)
 #define KC_AS LALT_T(KC_S)
 #define KC_CD LCTL_T(KC_D)
@@ -44,17 +45,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,  KC_MPLY,  KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
    KC_GA,  KC_AS,  KC_CD,  KC_SF,   KC_G,            KC_H,  KC_SJ,   KC_CK,  KC_AL, KC_GSCLN,
     KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,            KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH,
-                         KC_LCTL, KC_ENT,            KC_NUM_SPC, MO(_NAV)
+                      KC_RGUI, KC_SPC_LWR,            KC_SPC_RSE, KC_RALT
   ),
 
-  [_NUM_SYM] = LAYOUT(
+  [_LOWER] = LAYOUT(
         KC_1,  KC_2,     KC_3,     KC_4,      KC_5,  KC_TRNS,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
     KC_EXLM,  KC_AT,  KC_HASH,   KC_DLR,   KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR, KC_EQUAL,  KC_MINS,
     KC_BSLS,KC_LCBR,  KC_LBRC,  KC_LPRN,   KC_UNDS,            KC_RPRN,  KC_RBRC,  KC_RCBR,   KC_DOT,   KC_GRV,
                                 KC_CAPS,   KC_TRNS,            KC_TRNS,  KC_TRNS
   ),
 
-  [_NAV] = LAYOUT(
+  [_RAISE] = LAYOUT(
       RESET,  _______,  AG_NORM,  AG_SWAP,  DEBUG, KC_TRNS,   KC_GRV,  KC_PGDN,    KC_UP,  KC_PGUP,  KC_SCLN,
     RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI,  KC_NO,           KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_END,
     RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  KC_NO,           KC_MINS,    KC_RO,  KC_COMM,   KC_DOT,  KC_BSLS,
@@ -114,11 +115,11 @@ static void render_layer_status(void) {
         case _BASE:
             oled_write_ln_P(PSTR("BASE"), false);
             break;
-        case _NUM_SYM:
-            oled_write_ln_P(PSTR(" SYM"), false);
+        case _LOWER
+            oled_write_ln_P(PSTR("LOWR"), false);
             break;
-        case _NAV:
-            oled_write_ln_P(PSTR(" NAV"), false);
+        case _RAISE:
+            oled_write_ln_P(PSTR("RISE"), false);
             break;
         default:
             oled_write_ln_P(PSTR("?????"), false);
