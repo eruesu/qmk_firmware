a662ce682bc378a074d7cb3ac915460f899c467f
imlzo <56844000+imlzo@users.noreply.github.com>
2022-08-20 11:56:43 -0700

    le_chiffre: Update to new le_chiffre dir

--- keyboards/tominabox1/le_chiffre/keymaps/eruesu/keymap.c
+++ keyboards/tominabox1/le_chiffre/keymaps/eruesu/keymap.c
@@ -69,16 +69,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
-void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-}
-
 enum combo_events {
   COMBO_BSLASH,
   COMBO_QUOT,
