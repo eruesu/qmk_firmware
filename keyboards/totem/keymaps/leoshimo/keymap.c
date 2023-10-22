05cdbb4eff2a41614ae9170bdafe7a9b95fc2273
leoshimo <56844000+leoshimo@users.noreply.github.com>
2023-10-22 10:16:07 -0400

    feat: Totem supports media keys

--- keyboards/totem/keymaps/leoshimo/keymap.c
+++ keyboards/totem/keymaps/leoshimo/keymap.c
@@ -29,16 +29,12 @@ enum totem_layers {
     _LOWER,
     _RAISE,
     _ADJUST,
-}
-;
+};
 
 
 // ┌─────────────────────────────────────────────────┐
 // │ d e f i n e   m a c r o n a m e s               │
 // └─────────────────────────────────────────────────┘
-
-#define SPCLWR LT(_LOWER, KC_SPC)
-#define SPCRSE LT(_RAISE, KC_SPC)
 
 #define SFT_Z LSFT_T(KC_Z)
 #define SFT_SLSH RSFT_T(KC_SLSH)
@@ -97,12 +93,13 @@ uint16_t get_combo_term(uint16_t index, combo_t *combo) {
 // ▝▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▘
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
    [_QWERTY] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  
               LCTL_A,   KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,    
     LCTL_A,   SFT_Z,    KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   SFT_SLSH,  RSFT_T(KC_ENT),
-                                  KC_LALT,  KC_LGUI,  SPCLWR,    SPCRSE,   KC_RGUI,  KC_RALT
+                       KC_LGUI,  MO(_LOWER), KC_SPC,    KC_SPC,   MO(_RAISE),  KC_RALT
  ),
 
    [_LOWER] = LAYOUT(
@@ -124,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [_ADJUST] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
               QK_BOOT,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   
-              DB_TOGG,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,
+              DB_TOGG,  _______,  _______,  _______,  _______,   KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   _______,
                                   _______,  _______,  _______,   _______,  _______,  _______  
  )
